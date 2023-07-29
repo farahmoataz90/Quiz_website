e0046398cc9f6352194ddc7587d8422beed7038a
Farah Moataz <farahmoataz90@gmail.com>
2023-07-29 22:24:10 +0300

    updated

---
+++ questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions , options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor", 
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stands for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet", 
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stands for?",
+        answer: "HyperText Preprocessor",
+        options: [
+            "HyperText Preprocessor", 
+            "HyperText Programming",
+            "HyperText preprogramming",
+            "HomeText Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stands for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language", 
+            "Stylesheete Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stands for?",
+        answer: "eXtensible Markup language",
+        options: [
+            "eXtensible Markup language", 
+            "eXcutable Multiple Language",
+            "eXtra Multi-program Language",
+            "eXamine Multiple Language"
+        ]
+    }
+];
